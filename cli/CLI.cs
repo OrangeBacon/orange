f33b6333ed83ffc387bd84f3cd259a70faaae82d
Will Moreton <computer.backup.15@gmail.com>
2018-12-17 17:33:26 +0000

    Add solution and projects

---
+++ cli/CLI.cs
@@ -0,0 +1,13 @@
+﻿using vm;
+
+namespace CLI
+{
+    internal class CLI
+    {
+        private static void Main(string[] args)
+        {
+            Log.Info("Hello World!");
+            Log.Error("ERROR! ERROR!");
+        }
+    }
+}
---
+++ vm/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace vm
+{
+    public class Log
+    {
+        public static void Info(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString()}] INFO: {msg}");
+        }
+        public static void Warn(string msg)
+        {
+            Console.Write($"[{DateTime.Now.ToString()}] ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"WARN: {msg}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void Error(string msg)
+        {
+            Console.Error.Write($"[{DateTime.Now.ToString()}] ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine($"ERROR: {msg}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
