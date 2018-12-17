0117b3d4718bacc1a549ffcfc965df09f4525b30
Will Moreton <computer.backup.15@gmail.com>
2018-12-17 18:06:13 +0000

    Reformat cs

--- cli/CLI.cs
+++ cli/CLI.cs
@@ -1,11 +1,8 @@
 ﻿using vm;
 
-namespace CLI
-{
-    internal class CLI
-    {
-        private static void Main(string[] args)
-        {
+namespace CLI {
+    internal class CLI {
+        private static void Main(string[] args) {
             Log.Info("Hello World!");
             Log.Error("ERROR! ERROR!");
         }
