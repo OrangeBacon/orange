7b1f9fd5955a996352951f588967bae2094dfa42
Will Moreton <computer.backup.15@gmail.com>
2018-12-21 22:23:09 +0000

    Squirrel

--- Interface/App/App.xaml.cs
+++ Interface/App/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Squirrel;
 
 namespace Interface {
     /// <summary>
@@ -12,10 +13,17 @@ namespace Interface {
     /// </summary>
     public partial class App : Application {
         private void Application_Startup(object sender, StartupEventArgs e) {
+            Task.Run(Update);
             var win = new Views.MicrocodeProcess {
                 Title = "Starfish Virtual Machine"
             };
             win.Show();
+        }
+
+        private async Task Update() {
+            using (var mgr = UpdateManager.GitHubUpdateManager(@"https://github.com/ScratchOs/starfish")) { 
+                await mgr.Result.UpdateApp();
+            }
         }
     }
 }
