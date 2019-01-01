94121933ac5254557ca035ea42446eb1e3499477
Will Moreton <computer.backup.15@gmail.com>
2019-01-01 15:23:25 +0000

    update vmdesign, add memory

---
+++ VMLib/Memory64k.cs
@@ -0,0 +1,26 @@
+﻿using static VMLib.Util;
+using static VMLib.VMCore;
+
+namespace VMLib {
+    // linear 64kB memory
+    internal class Memory64k : Component {
+        // 65536 = 2^16
+        private readonly short[] memory = new short[65536];
+
+        public Memory64k(VMCore core, string name, LogicState<Bus> address, LogicState<Bus> output)
+            : base(core, nameof(Memory64k)) {
+            if(address.State != LogicState.In) {
+                Log.Warn("Can only use in state for memory address");
+            }
+            if(output.State != (LogicState.Out | LogicState.In)) {
+                Log.Warn("Can only use inout state for memory data");
+            }
+            Commands.Add(new MicrocodeCommand($"Memory({address.Value.Name}) -> {output.Value.Name}", () => {
+                output.Value.Write(memory[address.Value.Read()]);
+            }) { Depends = { this, address.Value }, Changes = { output.Value } });
+            Commands.Add(new MicrocodeCommand($"{output.Value.Name} -> Memory({address.Value.Name})", () => {
+                memory[address.Value.Read()] = output.Value.Read();
+            }) { Depends = { address.Value, output.Value }, Changes = { this } });
+        }
+    }
+}
--- VMLib/StarfishVM.cs
+++ VMLib/StarfishVM.cs
@@ -7,20 +7,29 @@ namespace VMLib {
         public static VMCore CreateStarfishVM() {
             // create default virtual machine
             var VM = new VMCore();
+            var phase = new PhaseCounter(VM);
 
             var dataBus = new Bus(VM, "Data");
             var leftBus = new Bus(VM, "ALU Left");
             var rightBus = new Bus(VM, "ALU Right");
+            var addrBus = new Bus(VM, "Address");
+            dataBus.Write(5);
+            addrBus.Write(7);
 
             var flags = new Register(VM, "flags");
             var ALU = new ALU(VM, leftBus, rightBus, dataBus, flags);
-            leftBus.Write(5);
-            rightBus.Write(7);
 
             var regA = new Register(VM, "A", InOutState(dataBus), OutState(leftBus));
-            var regB = new Register(VM, "B", InOutState(dataBus), OutState(rightBus));
+            var regB = new Register(VM, "B", InOutState(dataBus), OutState(leftBus));
+            var regC = new Register(VM, "C", InOutState(dataBus), OutState(rightBus));
+            var regD = new Register(VM, "D", InOutState(dataBus), OutState(rightBus));
+            var regE = new Register(VM, "E", InOutState(dataBus), OutState(rightBus));
 
-            var phase = new PhaseCounter(VM);
+            var regAR = new Register(VM, "AR", InOutState(dataBus), OutState(addrBus));
+            var regSP = new Register(VM, "SP", InOutState(dataBus), OutState(addrBus));
+            var regIP = new Register(VM, "IP", InOutState(dataBus), OutState(addrBus));
+
+            var mem = new Memory64k(VM, "Main Memory", InState(addrBus), InOutState(dataBus));
 
             return VM;
         }
