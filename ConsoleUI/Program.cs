2453e26cb64178d2cb8b98e1be0ff304737b5352
Will Moreton <computer.backup.15@gmail.com>
2019-03-19 15:39:48 +0000

    Add simple console ui for performance monitoring

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using VMLib;
+using static VMLib.StarfishVM;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+
+namespace ConsoleUI {
+    class Program {
+        static void Main(string[] args) {
+            var VM = CreateStarfishVM(new ConsoleLog());
+            var run = true;
+            Task.Run(delegate {
+                Thread.Sleep(10000);
+                run = false;
+            });
+            while(true) {
+                VM.Clock.RunCycle();
+                if(!run)
+                    break;
+            }
+            Console.WriteLine(VM.Components.OfType<PhaseCounter>().ElementAt(0).Phase);
+        }
+    }
+
+    class ConsoleLog : ILogger {
+        public void Error(string message) {
+            Console.Error.WriteLine(message);
+        }
+
+        public void Info(string message) {
+            Console.Write(message);
+        }
+
+        public void Warn(string message) {
+            Console.Write(message);
+        }
+    }
+}
