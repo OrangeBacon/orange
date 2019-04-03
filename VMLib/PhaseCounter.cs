67e4ec35026525cef565113bb7a090a9f6520b42
Will Moreton <computer.backup.15@gmail.com>
2019-04-03 18:22:52 +0100

    Close #7

--- VMLib/PhaseCounter.cs
+++ VMLib/PhaseCounter.cs
@@ -3,20 +3,16 @@
     public class PhaseCounter : Component {
         public ushort Phase { get; private set; } = 0;
 
-        private bool reset = false;
         public PhaseCounter(VMCore core) : base(core, "Phase Counter") {
             Commands.Add(new MicrocodeCommand("Reset Phase Counter", ()=> {
                 Phase = 0;
-                reset = true;
+                OnPropertyChanged("Phase");
             }));
 
             // run at start of clock sequence
             core.Clock.At(0).Add(() => {
                 // if reset, the counter should not be incremented
-                if(!reset) {
-                    Phase += 1;
-                }
-                reset = false;
+                Phase += 1;
                 OnPropertyChanged("Phase");
             });
         }
